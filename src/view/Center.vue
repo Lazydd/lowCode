f5c1f116276e48f10142b04bee0eeeddf50924a5
Lazydd <ddlazy@qq.com>
2022-12-21 10:44:08 +0800

    部分

--- src/view/Center.vue
+++ src/view/Center.vue
@@ -1,12 +1,41 @@
 <template>
     <div class="container">
         <main>
-            <el-scrollbar height="100%"> </el-scrollbar>
+            {{ list }}
+            <el-scrollbar height="100%">
+                <draggable
+                    animation="300"
+                    v-model="list"
+                    group="home"
+                    @start="drag = true"
+                    @end="drag = false"
+                    type="transition"
+                    :name="!drag ? 'flip-list' : null"
+                    item-key="id"
+                >
+                    <template #item="{ element }">
+                        <li>{{ element.name }}</li>
+                    </template>
+                </draggable>
+            </el-scrollbar>
         </main>
     </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, reactive } from "vue";
+import draggable from "vuedraggable";
+
+interface ListItem {
+    id: number;
+    name: string;
+}
+const drag = ref(false);
+const list = ref<ListItem[]>([
+    { id: 7, name: 7 },
+    { id: 8, name: 8 },
+]);
+</script>
 
 <style scoped lang="less">
 .container {
@@ -24,6 +53,14 @@
         bottom: 16px;
         border: 2px solid #197aff;
         overflow: hidden;
+        li {
+            padding: 15px;
+            background: #197aff;
+            margin-bottom: 10px;
+            -moz-user-select: none;
+            -webkit-user-select: none;
+            -ms-user-select: none;
+        }
     }
 }
 </style>
--- src/view/Left.vue
+++ src/view/Left.vue
@@ -1,110 +1,117 @@
 <template>
-	<el-scrollbar height="100vh">
-		<div class="page">
-			<el-input class="search" v-model="search" placeholder="搜索组件" :suffix-icon="Search" />
-			<el-collapse v-model="activeNames">
-				<el-collapse-item title="Consistency" name="1">
-					<div class="grid">
-						<li>1</li>
-						<li>2</li>
-						<li>3</li>
-						<li>4</li>
-					</div>
-				</el-collapse-item>
-				<el-collapse-item title="Consistency" name="2">
-					<div class="grid">
-						<li>1</li>
-						<li>2</li>
-						<li>3</li>
-						<li>4</li>
-					</div>
-				</el-collapse-item>
-				<el-collapse-item title="Consistency" name="3">
-					<div class="grid">
-						<li>1</li>
-						<li>2</li>
-						<li>3</li>
-						<li>4</li>
-					</div>
-				</el-collapse-item>
-				<el-collapse-item title="Consistency" name="4">
-					<div class="grid">
-						<li>1</li>
-						<li>2</li>
-						<li>3</li>
-						<li>4</li>
-					</div>
-				</el-collapse-item>
-				<el-collapse-item title="Consistency" name="5">
-					<div class="grid">
-						<li>1</li>
-						<li>2</li>
-						<li>3</li>
-						<li>4</li>
-					</div>
-				</el-collapse-item>
-			</el-collapse>
-		</div>
-	</el-scrollbar>
+    <el-scrollbar height="100vh">
+        <div class="page">
+            <el-input
+                class="search"
+                v-model="search"
+                placeholder="搜索组件"
+                :suffix-icon="Search"
+            />
+            <el-collapse v-model="activeNames">
+                <el-collapse-item title="Consistency" name="1">
+                    <draggable
+                        v-model="list"
+                        :group="{ name: 'home', pull: 'clone', put: false }"
+                        item-key="id"
+                        :sort="false"
+                        class="grid"
+                    >
+                        <template #item="{ element }">
+                            <li>{{ element.name }}</li>
+                        </template>
+                    </draggable>
+                </el-collapse-item>
+                <el-collapse-item title="Consistency" name="2">
+                    <draggable
+                        v-model="list"
+                        :group="{ name: 'home', pull: 'clone', put: false }"
+                        item-key="id"
+                        :sort="false"
+                        class="grid"
+                    >
+                        <template #item="{ element }">
+                            <li>{{ element.name }}</li>
+                        </template>
+                    </draggable>
+                </el-collapse-item>
+            </el-collapse>
+        </div>
+    </el-scrollbar>
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue';
-import {Search} from '@element-plus/icons-vue';
-const activeNames = ref(['1', '2', '3', '4', '5']);
-const search = ref('');
+import { ref, reactive } from "vue";
+import draggable from "vuedraggable";
+import { Search } from "@element-plus/icons-vue";
 
+interface ListItem {
+    id: number;
+    name: string;
+}
+
+const activeNames = ref(["1", "2", "3", "4", "5"]);
+const drag = ref(false);
+const list = ref<ListItem[]>([
+    { id: 1, name: 1 },
+    { id: 2, name: 2 },
+    { id: 3, name: 3 },
+    { id: 4, name: 4 },
+]);
+const search = ref("");
 </script>
 
 <style scoped lang="less">
 .page {
-	width: @_width;
-	height: 100%;
-	background: @_background;
-	.search {
-		margin: 12px 16px;
-	}
-	/deep/.el-input {
-		width: calc(100% - 32px);
-	}
-	.grid {
-		display: grid;
-		grid-template-columns: repeat(auto-fill, minmax(5rem, 1fr));
-		grid-auto-flow: row dense;
-		border: 1px solid #eaeefb;
-		border-bottom: 0;
-		> li {
-			display: flex;
-			flex-wrap: wrap;
-			flex-direction: column;
-			align-items: center;
-			justify-content: space-evenly;
-			text-align: center;
-			height: 114px;
-			cursor: pointer;
-			color: #666;
-			border-right: 1px solid #eaeaea;
-			border-bottom: 1px solid #eaeaea;
-			margin-bottom: -1px;
-			margin-right: -1px;
-			padding: 14px;
-			transition: all 0.5s;
-			&:hover {
-				box-shadow: 0 6px 16px 0 rgb(0 0 0 / 15%);
-				border-color: transparent;
-			}
-		}
-	}
-	/deep/.el-collapse {
-		-moz-user-select: none;
-		-webkit-user-select: none;
-		-ms-user-select: none;
-	}
-	/deep/.el-collapse-item__header {
-		padding-left: 16px;
-	}
-	/deep/.el-collapse-item__content {
-		padding-bottom: 0px;
-	}
+    width: @_width;
+    height: 100%;
+    background: @_background;
+    .search {
+        margin: 12px 16px;
+    }
+    :deep(.el-input) {
+        width: calc(100% - 32px);
+    }
+    .grid {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(5rem, 1fr));
+        grid-auto-flow: row dense;
+        // border: 1px solid #eaeefb;
+        border-bottom: 0;
+        > li {
+            display: flex;
+            flex-wrap: wrap;
+            flex-direction: column;
+            align-items: center;
+            justify-content: space-evenly;
+            text-align: center;
+            height: 114px;
+            cursor: pointer;
+            color: #666;
+            border: 1px solid #eaeaea;
+            // border-bottom: 1px solid #eaeaea;
+            margin-bottom: -1px;
+            margin-right: -1px;
+            padding: 14px;
+            transition: all 0.5s;
+            &:hover {
+                box-shadow: 0 6px 16px 0 rgb(0 0 0 / 15%);
+                border-color: transparent;
+            }
+        }
+    }
+    :deep(.el-collapse) {
+        -moz-user-select: none;
+        -webkit-user-select: none;
+        -ms-user-select: none;
+    }
+    :deep(.el-collapse-item__header) {
+        padding-left: 16px;
+    }
+    :deep(.el-collapse-item__content) {
+        padding-bottom: 0px;
+    }
+    .flip-list-move {
+        transition: transform 0.5s;
+    }
 }
 </style>
